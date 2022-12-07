f174b59edf69fdc5262d539fb932471e735bd796
Hatem Helal <hatemh@graphcore.ai>
2022-12-07 10:35:47 +0000

    reworked batching and lower LR

--- get-started/pyg_schnet_util.py
+++ get-started/pyg_schnet_util.py
@@ -4,12 +4,16 @@ import poptorch
 
 from torch_geometric.data import Batch, Data
 from torch_geometric.nn import to_fixed_size
-from torch_geometric.transforms import BaseTransform, Compose
 
 
 class TrainingModule(torch.nn.Module):
+    """
+    TrainingModule for SchNet.  Assumes that each mini-batch contains a single
+    padding molecule at the end and uses this to calculate the mean squared
+    error (MSE) for the real molcules in each mini-batch.
+    """
 
-    def __init__(self, module, batch_size) -> None:
+    def __init__(self, module, batch_size):
         super().__init__()
         self.model = to_fixed_size(module=module, batch_size=batch_size)
 
@@ -17,21 +21,12 @@ class TrainingModule(torch.nn.Module):
         args = [t.squeeze(0) for t in args]
         model_args, target = args[0:-1], args[-1]
         prediction = self.model(*model_args).view(-1)
-        loss = self.mse_loss(prediction, target)
+
+        # slice off the padding molecule and calculate the mse loss
+        prediction = prediction[0:-1]
+        target = target[0:-1]
+        loss = F.mse_loss(prediction, target)
         return prediction, loss
-
-    @staticmethod
-    def mse_loss(input, target):
-        """
-        Calculates the mean squared error
-
-        This loss assumes that zeros are used as padding on the target so that
-        the count can be derived from the number of non-zero elements.
-        """
-        loss = F.mse_loss(input, target, reduction="sum")
-        N = (target != 0.0).to(loss.dtype).sum()
-        loss = loss / N
-        return poptorch.identity_loss(loss, reduction="none")
 
 
 class KNNInteractionGraph(torch.nn.Module):
@@ -77,81 +72,27 @@ def arange_like(n: int, ref: torch.Tensor) -> torch.Tensor:
     return torch.arange(n, device=ref.device, dtype=ref.dtype)
 
 
-class PadMolecule(BaseTransform):
+def prepare_data(data):
     """
-    Data transform that applies padding to enforce consistent tensor shapes.
+    Prepares QM9 molecules for training SchNet for HOMO-LUMO gap prediction
+    task.  Outputs a data object with attributes:
 
-    Padding atoms are defined as have atomic charge of zero and are placed at a 
-    distance of 10 * cutoff ^ 2 to ensure there are no edges created between 
+        z: the atomic number as a vector of integers with length [num_atoms]
+        pos: the atomic position as a [num_atoms, 3] tensor of float32 values.
+        y: the HOMO-LUMO gap energy in electronvolts (eV).
     """
-
-    def __init__(self, max_num_atoms: int, cutoff: float):
-        """
-        :param max_num_atoms (int): The maximum number of atoms to pad
-            the atomic numbers and position tensors to.
-        :param cutoff (float): The cutoff in Angstroms used in the SchNet model.
-        """
-        super().__init__()
-        self.max_num_atoms = max_num_atoms
-        self.cutoff = cutoff
-
-    def validate(self, data):
-        """
-        Validates that the input molecule does not exceed the constraint that
-        the number of atoms must be <= max_num_atoms
-        """
-        num_atoms = data.z.numel()
-
-        assert num_atoms <= self.max_num_atoms, \
-            f"Too many atoms. Molecule has {num_atoms} atoms "\
-            f"and max_num_atoms is {self.max_num_atoms}."
-
-        return num_atoms
-
-    def __call__(self, data):
-        num_atoms = self.validate(data)
-        num_fake_atoms = self.max_num_atoms - num_atoms
-        data.z = F.pad(data.z, (0, num_fake_atoms), 'constant', 0)
-        data.pos = F.pad(data.pos, (0, 0, 0, num_fake_atoms), 'constant',
-                         10. * self.cutoff**2)
-        data.num_nodes = self.max_num_atoms
-        return data
+    gap_target = 4
+    return Data(z=data.z, pos=data.pos, y=data.y[0, gap_target].view(-1))
 
 
-class PrepareData(BaseTransform):
+def padding_graph(num_nodes):
     """
-    Data transform for preparing each data instance by:
-        * extracting a given target label from the PyG QM9 dataset
-        * slicing the data objet to only contain the provided properties.
-
-    The QM9 dataset consists of a total of 19 regression targets. This transform
-    indexes the regression targets stored in data.y to only include the selected
-    target.
-
-    Expected input:
-        data.y is a vector with shape [1, 19]
-
-    Transformed output:
-        data.y is as scalar with shape torch.Size([])
+    Create a molecule of non-interacting atoms defined as having atomic charge
+    of zero to use for padding a mini-batch up to a known maximum size
     """
-
-    def __init__(self, target, keys):
-        self.target = target
-        self.keys = keys
-
-    def validate(self, data):
-        assert hasattr(data, "y") \
-          and isinstance(data.y, torch.Tensor) \
-          and data.y.shape == (1, 19),\
-          "Invalid data input. Expected data.y == Tensor with shape [1, 19]"
-
-    def __call__(self, data):
-        self.validate(data)
-        data.y = data.y[0, self.target]
-
-        values = [getattr(data, k) for k in self.keys]
-        kwargs = dict([*zip(self.keys, values)])
-        return Data(**kwargs)
+    return Data(z=torch.zeros(num_nodes, dtype=torch.long),
+                pos=torch.zeros(num_nodes, 3),
+                y=torch.zeros(1))
 
 
 class CombinedBatchingCollator:
@@ -163,7 +104,7 @@ class CombinedBatchingCollator:
     This is intended to be used in combination with the poptorch.DataLoader
     """
 
-    def __init__(self, mini_batch_size, keys):
+    def __init__(self, mini_batch_size, keys, max_nodes_per_graph):
         """
         :param mini_batch_size (int): mini batch size used by the SchNet model
         :param keys: Keys to include from the batch in the
@@ -173,6 +114,13 @@ class CombinedBatchingCollator:
         super().__init__()
         self.mini_batch_size = mini_batch_size
         self.keys = keys
+        self.max_nodes_per_batch = max_nodes_per_graph * (mini_batch_size + 1)
+
+    def add_padding_graph(self, data_list):
+        num_nodes = sum(d.num_nodes for d in data_list)
+        num_pad_nodes = self.max_nodes_per_batch - num_nodes
+        data_list.append(padding_graph(num_pad_nodes))
+        return data_list
 
     def batch_to_tuple(self, data_list):
         batch = Batch.from_data_list(data_list)
@@ -190,6 +138,7 @@ class CombinedBatchingCollator:
 
         for i in range(num_mini_batches):
             slices = batch[start:start + stride]
+            slices = self.add_padding_graph(slices)
             batches[i] = self.batch_to_tuple(slices)
             start += stride
 
@@ -202,30 +151,10 @@ class CombinedBatchingCollator:
         return tuple(outputs)
 
 
-def create_transform(cutoff: float, max_num_atoms):
-    """
-    Creates a sequence of transforms defining a data pre-processing pipeline
-
-    :param cutoff (float): Cutoff distance for interatomic interactions in
-        Angstroms (default: 6.0).
-    :param max_num_atoms (int): The maximum number of atoms used by the
-        PadMolecule transform (default: 32).
-
-    :returns: A composite transform
-    """
-    # The HOMO-LUMO gap is target 4 in QM9
-    target = 4
-    keys = ("z", "pos", "y", "num_nodes")
-
-    return Compose([
-        PadMolecule(cutoff=cutoff, max_num_atoms=max_num_atoms),
-        PrepareData(target=target, keys=keys)
-    ])
-
-
 def create_dataloader(dataset,
-                      ipu_opts,
-                      batch_size=1,
+                      ipu_opts=poptorch.Options(),
+                      batch_size=2,
+                      max_nodes_per_graph=32,
                       shuffle=False,
                       num_workers=0):
     """
@@ -233,16 +162,17 @@ def create_dataloader(dataset,
     Applies the mini-batching method of concatenating multiple graphs into a 
     single graph with multiple disconnected subgraphs. See:
     https://pytorch-geometric.readthedocs.io/en/2.0.2/notes/batching.html
+    
+    Automatically adds a padding graph to fill up each mini-batch up to contain
+    max_nodes_per_graph * (batch_size-1) nodes.
     """
-    if ipu_opts is None:
-        ipu_opts = poptorch.Options()
-
     keys = ("z", "pos", "batch", "y")
-    collater = CombinedBatchingCollator(batch_size, keys)
+    collater = CombinedBatchingCollator(batch_size - 1, keys,
+                                        max_nodes_per_graph)
 
     return poptorch.DataLoader(ipu_opts,
                                dataset=dataset,
-                               batch_size=batch_size,
+                               batch_size=batch_size - 1,
                                shuffle=shuffle,
                                num_workers=num_workers,
                                collate_fn=collater)
