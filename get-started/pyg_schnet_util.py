f3a64278fb54968bb3c95d5f2ad60a88f74cd6d4
Hatem Helal <hatemh@graphcore.ai>
2022-12-08 16:17:44 +0000

    add additional optimisations

--- get-started/pyg_schnet_util.py
+++ get-started/pyg_schnet_util.py
@@ -6,6 +6,27 @@ from torch_geometric.data import Batch, Data
 from torch_geometric.nn import to_fixed_size
 
 
+class ShiftedSoftplus(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.shift = torch.log(torch.tensor(2.0)).item()
+
+    def forward(self, x):
+        u = torch.log1p(torch.exp(-x.abs()))
+        v = torch.clamp_min(x, 0.)
+        return u + v - self.shift
+
+    @staticmethod
+    def replace_activation(module: torch.nn.Module):
+        import torch_geometric.nn.models.schnet as pyg_schnet
+        for name, child in module.named_children():
+            if isinstance(child, pyg_schnet.ShiftedSoftplus):
+                setattr(module, name, ShiftedSoftplus())
+            else:
+                ShiftedSoftplus.replace_activation(child)
+
+
 class TrainingModule(torch.nn.Module):
     """
     TrainingModule for SchNet.  Assumes that each mini-batch contains a single
@@ -13,8 +34,11 @@ class TrainingModule(torch.nn.Module):
     error (MSE) for the real molcules in each mini-batch.
     """
 
-    def __init__(self, module, batch_size):
+    def __init__(self, module, batch_size, replace_softplus=True):
         super().__init__()
+        if replace_softplus:
+            ShiftedSoftplus.replace_activation(module)
+
         self.model = to_fixed_size(module=module, batch_size=batch_size)
 
     def forward(self, *args):
@@ -70,6 +94,18 @@ class KNNInteractionGraph(torch.nn.Module):
 
 def arange_like(n: int, ref: torch.Tensor) -> torch.Tensor:
     return torch.arange(n, device=ref.device, dtype=ref.dtype)
+
+
+def optimize_popart(options):
+    """Apply a number of additional PopART options to optimize performace"""
+    options._Popart.set('defaultBufferingDepth', 4)
+    options._Popart.set("accumulateOuterFragmentSettings.schedule", 2)
+    options._Popart.set(
+        "replicatedCollectivesSettings.prepareScheduleForMergingCollectives",
+        True)
+    options._Popart.set(
+        "replicatedCollectivesSettings.mergeAllReduceCollectives", True)
+    return options
 
 
 def prepare_data(data, target=4):
