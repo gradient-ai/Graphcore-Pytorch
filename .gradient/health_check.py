ddd37109f87ef9f53964e3111db1ae20d0d5a77d
Eva Woodbridge <evaw@graphcore.ai>
2023-03-27 16:45:15 +0100

    Use logger

--- .gradient/health_check.py
+++ .gradient/health_check.py
@@ -3,11 +3,14 @@ import subprocess
 import json
 import os
 import yaml
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 # Check that the datasets have mounted as expected
 
 # Gather the datasets expected from the settings.yaml
-with open('settings.yaml') as f:
+with open("settings.yaml") as f:
     my_dict = yaml.safe_load(f)
     datasets = my_dict["integrations"].keys()
 
@@ -18,7 +21,7 @@ def check_files_exist(files: [str], dirname: str):
     for filename in files:
         full_path = str(dirpath/filename)
         if full_path not in sub_directories:
-            print(filename + " not found")
+            logging.warning(filename + " not found in " + dirname)
 
 # Check that dataset exists and
 check_files_exist(datasets, "/datasets")
@@ -27,16 +30,16 @@ check_files_exist(datasets, "/datasets")
 # Do not need to run full hash checks
 
 # Check that files are symlinked correctly
-expected_exe_cache = ["fine-tuning-bert", "kge_training"]
-check_files_exist(expected_files, "/tmp/exe_cache")
+expected_exe_cache = ["fine-tuning-bert", "kge_training", "not_here"]
+check_files_exist(expected_exe_cache, "/tmp/exe_cache")
 
 #Check that the number of detected IPUs is correct
 pod_type = os.getenv("GRAPHCORE_POD_TYPE")
 expected_ipu_num = pod_type.replace("pod","")
 
-num_ipus = os.getenv("NUM_AVAILABLE_IPU")
+num_ipus = os.getenv("NUM_AVAILABLE_IPU", "0")
 
 if expected_ipu_num != num_ipus:
-    print("Incorrect number of IPUs found "+ num_ipus+" expected "+ expected_ipu_num)
+    logger.warning("Incorrect number of IPUs found "+ num_ipus+" expected "+ expected_ipu_num)
 else:
-    print("Correct number IPUs found")
+    logger.info("Correct number IPUs found")
