2d588d09e6fe2d2d04dd37f905ce9101af4a7c40
Eva Woodbridge <evaw@graphcore.ai>
2023-04-03 16:48:40 +0100

    Add metadata file checking

--- .gradient/health_check.py
+++ .gradient/health_check.py
@@ -4,6 +4,8 @@ import json
 import os
 import yaml
 import logging
+from examples_utils.paperspace_utils.dataset_upload_checker import check_files_match_metadata
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,22 +16,25 @@ with open("settings.yaml") as f:
     my_dict = yaml.safe_load(f)
     datasets = my_dict["integrations"].keys()
 
+
 def check_files_exist(files: [str], dirname: str):
     dirpath = Path(dirname)
     sub_directories = [str(f) for f in dirpath.iterdir() if f.is_dir()]
-    print(sub_directories)
     for filename in files:
         full_path = str(dirpath/filename)
         if full_path not in sub_directories:
             logging.warning(filename + " not found in " + dirname)
+        else:
+            dataset_sub_directories = [str(f) for f in Path(full_path).iterdir()]
+            if full_path+"/gradient_dataset_metadata.json" in dataset_sub_directories:
+                check_files_match_metadata(full_path,False)
+            else:
+                logging.warning("Metadata file not found in "+ full_path)
 
-# Check that dataset exists and
-check_files_exist(datasets, "/datasets")
+# Check that dataset exists and if a metadata file is found check that all files in the metadata file exist
+check_files_exist(datasets, "./datasets")
 
-# Using script from examples-utils check that the metadata files are correct
-# Do not need to run full hash checks
-
-# Check that files are symlinked correctly
+# Check that files are symlinked correctly - this needs to be manually edited for each runtime
 expected_exe_cache = ["fine-tuning-bert", "kge_training", "not_here"]
 check_files_exist(expected_exe_cache, "/tmp/exe_cache")
 
