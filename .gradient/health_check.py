058f0250e38b41a5c795869c7bf9d6afb5192d32
Eva Woodbridge <evaw@graphcore.ai>
2023-04-04 14:55:37 +0100

    Adding main function

--- .gradient/health_check.py
+++ .gradient/health_check.py
@@ -6,17 +6,6 @@ import yaml
 import logging
 from examples_utils.paperspace_utils.dataset_upload_checker import check_files_match_metadata
 
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-# Check that the datasets have mounted as expected
-
-# Gather the datasets expected from the settings.yaml
-with open("settings.yaml") as f:
-    my_dict = yaml.safe_load(f)
-    datasets = my_dict["integrations"].keys()
-
-
 def check_files_exist(files: [str], dirname: str):
     dirpath = Path(dirname)
     sub_directories = [str(f) for f in dirpath.iterdir() if f.is_dir()]
@@ -31,20 +20,33 @@ def check_files_exist(files: [str], dirname: str):
             else:
                 logging.warning("Metadata file not found in "+ full_path)
 
-# Check that dataset exists and if a metadata file is found check that all files in the metadata file exist
-check_files_exist(datasets, "./datasets")
-
-# Check that files are symlinked correctly - this needs to be manually edited for each runtime
-expected_exe_cache = ["fine-tuning-bert", "kge_training", "not_here"]
-check_files_exist(expected_exe_cache, "/tmp/exe_cache")
-
 #Check that the number of detected IPUs is correct
-pod_type = os.getenv("GRAPHCORE_POD_TYPE")
-expected_ipu_num = pod_type.replace("pod","")
+def check_num_pod_expected(logger:logging.Logger):
+    pod_type = os.getenv("GRAPHCORE_POD_TYPE")
+    expected_ipu_num = pod_type.replace("pod","")
 
-num_ipus = os.getenv("NUM_AVAILABLE_IPU", "0")
+    num_ipus = os.getenv("NUM_AVAILABLE_IPU", "0")
 
-if expected_ipu_num != num_ipus:
-    logger.warning("Incorrect number of IPUs found "+ num_ipus+" expected "+ expected_ipu_num)
-else:
-    logger.info("Correct number IPUs found")
+    if expected_ipu_num != num_ipus:
+        logger.warning("Incorrect number of IPUs found "+ num_ipus+" expected "+ expected_ipu_num)
+    else:
+        logger.info("Correct number IPUs found")
+
+def main():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    # Check that the datasets have mounted as expected
+
+    # Gather the datasets expected from the settings.yaml
+    with open("settings.yaml") as f:
+        my_dict = yaml.safe_load(f)
+        datasets = my_dict["integrations"].keys()
+
+    # Check that dataset exists and if a metadata file is found check that all files in the metadata file exist
+    check_files_exist(datasets, "./datasets")
+
+    # Check that files are symlinked correctly - this needs to be manually edited for each runtime
+    expected_exe_cache = ["fine-tuning-bert", "kge_training"]
+    check_files_exist(expected_exe_cache, "/tmp/exe_cache")
+
+main()
